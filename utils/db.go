afeaf75979783a3af0b2b9560775c0c7a365b2ac
sumit3105 <gohilsumit15@gmail.com>
2025-12-30 10:25:39 +0530

    Refactored code in db connection file

--- utils/db.go
+++ utils/db.go
@@ -27,7 +27,7 @@ func Connect() *gorm.DB {
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		logger.Log.Error("Failed to connect to DB : " + err.Error(), zap.Error(err), zap.String("dsn", dsn), zap.Int64("time", time.Now().Unix()))
+		logger.Log.Error("Failed to connect to DB : " + err.Error(), zap.Error(err), zap.String("dsn", dsn), zap.Time("time", time.Now()))
 		panic("Failed to connect to database")
 	}
 
@@ -38,7 +38,7 @@ func PerformMigration(db *gorm.DB) {
 	err := db.AutoMigrate(&model.User{}, &model.Address{})
 
 	if err != nil {
-		logger.Log.Error("Migration failed : " + err.Error(), zap.Error(err), zap.Int64("time", time.Now().Unix()))
+		logger.Log.Error("Migration failed : " + err.Error(), zap.Error(err), zap.Time("time", time.Now()))
 		panic("Migration failed")
 	}
 }
